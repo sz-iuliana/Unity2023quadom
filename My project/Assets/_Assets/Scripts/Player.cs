ef3ba32546629d224ab4e1d4e78ccb308b8242fe
szekeres iuliana <szekeres.sz.j@gmail.com>
2023-06-09 07:40:40 +0300

    Animation

--- My project/Assets/_Assets/Scripts/Player.cs
+++ My project/Assets/_Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
     private void Update()
     {
        
@@ -40,12 +41,20 @@ public class NewBehaviourScript : MonoBehaviour
         inputVector=inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
+        
         transform.position += moveDir*Time.deltaTime* moveSpeed;
+
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         Debug.Log(Time.deltaTime);
 
-    }
+       
 
+    }
+   public bool IsWalking()
+    {
+        return isWalking;
+
+    }
 }
---
+++ My project/Assets/_Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    private Animator animator;
+    [SerializeField] private NewBehaviourScript player;
+    private void Awake()
+    {
+        animator = getComponent<Animator>();
+        
+
+    }
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
